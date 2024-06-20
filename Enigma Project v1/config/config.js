abf02244d4a12f1968253d916014f328b1db9567
ZdragunTheReal <106932615+ZdragunTheReal@users.noreply.github.com>
2024-06-20 21:45:30 +0200

    Update config.js

--- Enigma Project v1/config/config.js
+++ Enigma Project v1/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  token: "MTI1MzQxNzg2NjgyMTEwNzgwMg.GMcUod.xKG0tyqlR6ylLsEl9aEpre1Jo0Uq0SDTu2-NWQ",
-  botId: "1240348188314046548",
+  token: "MTI1MzQxNzg2NjgyMTEwNzgwMg.GRuPLP.lNMOOnoa_gydYB87rpTfBf3V1ZDPBkFDzMcyPE",
+  botId: "1253417866821107802",
   buyers: ["551360617961881612"],
   prefix: "+",
   color: "#2b2d31",
